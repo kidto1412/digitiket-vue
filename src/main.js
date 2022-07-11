e2f2646d8e0b733b30748408e2d1c28bc8efbff3
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-07-11 21:43:26 +0700

    starter project

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { BootstrapVue } from 'bootstrap-vue'
+import axios from './plugins/axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  BootstrapVue,
+  axios,
+  VueAxios,
+  render: (h) => h(App),
 }).$mount('#app')
