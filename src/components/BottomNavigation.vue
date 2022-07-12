b966d29566c4921e2e6e3305698613cd82add89c
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-07-12 18:08:26 +0700

    add component Bottom Navigation

---
+++ src/components/BottomNavigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-bottom-navigation :value="value" color="purple">
+    <v-btn>
+      <span>Beranda</span>
+
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Lokasi</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Tiket</span>
+      <v-icon>mdi-ticket-confirmation</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Akun</span>
+
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+<script>
+export default {
+  name: 'BottomNavigation',
+  data: () => ({ value: 0 }),
+}
+</script>
+<style scoped>
+.v-item-group.v-bottom-navigation {
+  position: fixed;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <bottom-navigation />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BottomNavigation from '../components/BottomNavigation.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    BottomNavigation,
   },
 }
 </script>
