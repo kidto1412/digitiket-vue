c3b9795c124ea6dfa5122c4263af4bcc7ed99545
Rico Chandra <ricochandraofficial@gmail.com>
2022-07-15 18:14:23 +0700

    Add Promo Terkini

---
+++ src/views/PromoTerkini.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card style="height: 100vh;">
+    <v-row class="vrow">
+        <v-col
+        v-for="n in 2"
+        :key="n"
+        class="d-flex child-flex"
+        cols="11"
+        >
+        <v-img
+            :src="require(`../../public/img/dor${n}.png`)"
+            :lazy-src="require(`../../public/img/dor${n}.png`)"
+            aspect-ratio="1"
+            class="grey lighten-2 img"
+        >
+            <template v-slot:placeholder>
+            <v-row
+                class="fill-height ma-0"
+            >
+                <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+                ></v-progress-circular>
+            </v-row>
+            </template>
+        </v-img>
+        </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<style scoped>
+    .vrow{
+        display: flex;
+        flex-wrap: wrap;
+        flex: 1 1 auto;
+        margin: -12px;
+        align-content: space-between;
+        justify-content: space-evenly;
+    }
+    .img{
+        border-radius: 10px;
+    }
+</style>
+
